13198585cdf8d2664deb63dfb235613db3460762
LurchingDart <luizalferez25@gmail.com>
2024-01-24 21:34:15 -0600

    Hot Fix
    Added a simple text to all pages to avoid Prerender Errors

---
+++ src/app/layered/page.jsx
@@ -0,0 +1,8 @@
+export default function ClientServer() {
+
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+            <h1 className="tracking-tight inline font-semibold text-8xl bg-clip-text text-transparent bg-gradient-to-b from-[#FF1CF7] to-[#b249f8]">Layered</h1>
+        </div>
+    );
+}
