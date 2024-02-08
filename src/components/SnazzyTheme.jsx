61529817e61460f623b69f8f023a3bbabdbf2112
LurchingDart <luizalferez25@gmail.com>
2024-02-07 20:03:13 -0600

    Add code styling component and clean up the navigation bar
    new code styling component, SnazzyTheme, used in several components to highlight code syntax. Additionally, the navigation bar has been cleaned up by removing unused elements

---
+++ src/components/SnazzyTheme.jsx
@@ -0,0 +1,36 @@
+const Snazzy = {
+    'hljs': {
+        'backgroundColor': '#282a36',
+        'color': '#eff0eb',
+    },
+    'hljs-comment': { 'color': '#686868' },
+    'hljs-quote': { 'color': '#686868' },
+    'hljs-variable': { 'color': '#ff5c57' },
+    'hljs-template-variable': { 'color': '#ff5c57' },
+    'hljs-attribute': { 'color': '#ff5c57' },
+    'hljs-tag': { 'color': '#EFDDFC' },
+    'hljs-name': { 'color': '#DDBDFA' },
+    'hljs-regexp': { 'color': '#ff5c57' },
+    'hljs-link': { 'color': '#ff5c57' },
+    'hljs-selector-id': { 'color': '#ff5c57' },
+    'hljs-selector-class': { 'color': '#ff5c57' },
+    'hljs-number': { 'color': '#5af78e' },
+    'hljs-meta': { 'color': '#5af78e' },
+    'hljs-built_in': { 'color': '#5af78e' },
+    'hljs-builtin-name': { 'color': '#5af78e' },
+    'hljs-literal': { 'color': '#5af78e' },
+    'hljs-type': { 'color': '#5af78e' },
+    'hljs-params': { 'color': '#5af78e' },
+    'hljs-string': { 'color': '#5af78e' },
+    'hljs-symbol': { 'color': '#f3f99d' },
+    'hljs-bullet': { 'color': '#f3f99d' },
+    'hljs-title': { 'color': '#57c7ff' },
+    'hljs-section': { 'color': '#57c7ff' },
+    'hljs-keyword': { 'color': '#ff6ac1' },
+    'hljs-deletion': { 'color': '#ff6ac1' },
+    'hljs-addition': { 'color': '#9aedfe' },
+    'hljs-emphasis': { 'color': '#9aedfe' },
+    'hljs-strong': { 'color': '#9aedfe' }
+};
+
+export default Snazzy;
