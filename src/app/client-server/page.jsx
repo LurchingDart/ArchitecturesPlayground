ae6e83e46eb1ed7a7d6c35fff61184c31c14eecf
Luis Alférez <lurchdev@gmail.com>
2024-02-25 17:24:45 -0600

    Add client-server description

--- src/app/client-server/page.jsx
+++ src/app/client-server/page.jsx
@@ -3,6 +3,7 @@ export default function ClientServer() {
     return (
         <div className="flex h-full w-full items-center justify-center">
             <h1 className="tracking-tight inline font-semibold text-8xl bg-clip-text text-transparent bg-gradient-to-b from-[#FF1CF7] to-[#b249f8]">Client-Server</h1>
+            <p className="text-2xl">This is a client-server page</p>
         </div>
     );
 }
